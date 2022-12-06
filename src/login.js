16fb68118177121658a65dcc26af8f8eb413eb3d
Angy Encinas <angy.encinas@ucb.edu.bo>
2022-12-06 09:43:45 -0400

    la constraseña tiene una mayuscula al inicio

--- src/login.js
+++ src/login.js
@@ -1,15 +1,20 @@
 function verificarContrasena(password) {
   var resultado = " ";
   var tamaño = password.length;
+  var primerCaracter = password.charAt(0);
+  var conversionAMayuscula = primerCaracter.toUpperCase();
   if(password == " " || password == ""){
     resultado = "La contraseña no debe ser nula";
   }
   else{
-    if(tamaño > 8){
+    if(tamaño > 8 && primerCaracter == conversionAMayuscula){
       resultado = "Valido";
     }
-    else{
+    else if(tamaño < 8) {
       resultado = "La contraseña debe tener mas de 8 caracteres";
+    }
+    else if( primerCaracter != conversionAMayuscula){
+      resultado = "La contraseña debe al menos una letra mayuscula";
     }
   }
   
--- src/login.test.js
+++ src/login.test.js
@@ -2,12 +2,18 @@ import verificarContrasena from "./login.js";
 
 describe("Verificar contraseña", () => {
   it("deberia tener mas de 8 caracteres", () => {
-    expect(verificarContrasena("alejandra")).toEqual("Valido");
+    expect(verificarContrasena("Alejandra")).toEqual("Valido");
   });
   it("No deberia tener mas de 8 caracteres", () => {
     expect(verificarContrasena("ale")).toEqual("La contraseña debe tener mas de 8 caracteres");
   });
   it("No debe ser nula", () => {
     expect(verificarContrasena(" ")).toEqual("La contraseña no debe ser nula");
+  });
+  it("Deberia tener una mayuscula", () => {
+    expect(verificarContrasena("Alejandra")).toEqual("Valido");
+  });
+  it("No deberia tener una mayuscula", () => {
+    expect(verificarContrasena("alejandra")).toEqual("La contraseña debe al menos una letra mayuscula");
   });
 });
